37d843094811a0c8fe8f27324dea13fe355a92ea
Aayush Soni <44709621+Aayushsoni09@users.noreply.github.com>
2025-04-04 23:14:02 +0530

    Update install_app.sh to change the permissions

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -4,4 +4,6 @@ set -e
 echo "=== Installing app dependencies ==="
 cd /var/www/nextjsproject
 npm install --production || { echo "❌ npm install failed"; exit 1; }
+chown -R ec2-user:ec2-user .  # Ensure ec2-user owns everything
+chmod -R u+rw .  # Ensure write permissions
 echo "✅ App dependencies installed!"
