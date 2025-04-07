5fa45edbfc8221f4c3ef0e80f7b21a4fc5030b84
Aayush Soni <44709621+Aayushsoni09@users.noreply.github.com>
2025-04-07 13:37:33 +0530

    Update and rename next.config.mjs to next.config.js

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = new URL('./src', import.meta.url).pathname;
-    return config;
-  },
-};
-
-export default nextConfig;
