cc4eaae88c373d64104f26465c00fd0e94620bd1
Aayush Soni <44709621+Aayushsoni09@users.noreply.github.com>
2025-04-09 00:41:45 +0530

    Update install_app.sh

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -7,3 +7,4 @@ npm install --production || { echo "❌ npm install failed"; exit 1; }
 chown -R ec2-user:ec2-user .  # Ensure ec2-user owns everything
 chmod -R u+rw .  # Ensure write permissions
 echo "✅ App dependencies installed!"
+#this is an updated comment
