06f10a138187d01b3a7ad7a37d3a1e1cb02065b7
Aayush Soni <44709621+Aayushsoni09@users.noreply.github.com>
2025-04-09 12:53:28 +0530

    Update install_app.sh

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -4,7 +4,7 @@ set -e
 echo "=== Installing app dependencies ==="
 cd /var/www/nextjsproject
 npm install --production || { echo "❌ npm install failed"; exit 1; }
-chown -R ec2-user:ec2-user .  # Ensure ec2-user owns everything
-chmod -R u+rw .  # Ensure write permissions
+sudo chown -R ec2-user:ec2-user /var/www/nextjsproject  # Ensure ec2-user owns everything
+sudo chmod -R u+rw /var/www/nextjsproject  # Ensure write permissions
 echo "✅ App dependencies installed!"
 
