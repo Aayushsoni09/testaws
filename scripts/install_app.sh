4ed5cadaeb192bad3df51dd3dc2ef10764bd2f46
Aayush Soni <44709621+Aayushsoni09@users.noreply.github.com>
2025-04-10 11:15:00 +0530

    Update install_app.sh

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -3,11 +3,9 @@ set -e
 
 echo "=== Installing app dependencies ==="
 cd /var/www/nextjsproject || { echo "❌ Failed to enter /var/www/nextjsproject"; exit 1; }
-
+npm install --production || { echo "❌ npm install failed"; exit 1; }
 # Forcefully change ownership to ec2-user
 sudo chown -R ec2-user:ec2-user /var/www/nextjsproject
-
-npm install --production || { echo "❌ npm install failed"; exit 1; }
-sudo chmod -R u+rw /var/www/nextjsproject
-
 echo "✅ App dependencies installed!"
+
+
